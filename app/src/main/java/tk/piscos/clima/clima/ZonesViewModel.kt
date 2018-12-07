b689538fd24c09b156edd580eb204dbf2e8aac71
miguel <mig.ruiz@gmail.com>
2018-12-07 13:18:37 +0000

    refactoring model

--- app/src/main/java/tk/piscos/clima/clima/ZonesViewModel.kt
+++ app/src/main/java/tk/piscos/clima/clima/ZonesViewModel.kt
@@ -3,10 +3,7 @@ package tk.piscos.clima.clima
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class ZonesViewModel(application: Application): AndroidViewModel(application)  {
 
@@ -19,47 +16,68 @@ class ZonesViewModel(application: Application): AndroidViewModel(application)  {
     private val mqttClient= MQTTClient("tcp://piscos.tk:1883")
     fun fetchData() {
         GlobalScope.launch(Dispatchers.Main) {
-            async { mqttClient.connectAsync(getApplication()) }.await()
-            val zonesClimatelist = async {
-                mqttClient.getResponse<List<ZoneClimateData>>(
-                    requestTopic = "AllZonesReadingsRequest",
-                    responseTopic = "AllZonesReadingResponse"
-                )
-            }.await()
-            val zonesBoilerlist = async {
-                mqttClient.getResponse<List<ZoneBoilerData>>(
-                    requestTopic = "AllZonesConfigRequest",
-                    responseTopic = "AllZonesConfigResponse"
-                )
-            }.await()
-            val modelList = zonesClimatelist.map {
-                ZoneCellModel(
-                    temperature = it.temperature,
-                    humidity = it.humidity,
-                    coverage = it.coverage,
-                    zoneCode = it.zoneCode
-                )
-            }
-            zonesBoilerlist.forEach {
-                val zoneCellModel=modelList.first{a-> a.zoneCode.equals(it.zoneCode,true)}
-                zoneCellModel.regulated=it.regulated
-                zoneCellModel.targetTemperature=it.targetTemperature
-            }
-
+            val  modelList = getModel()
             zones.value = modelList
-            async {
-                mqttClient.subscribe<ZoneClimateData>("zoneClimateChange") {
-                    val zoneModel=this@ZonesViewModel.zones.value!!.firstOrNull{a-> a.zoneCode.equals(it.zoneCode,true)}
-                    if (zoneModel!=null) {
-                        zoneModel.temperature = it.temperature
-                        zoneModel.coverage = it.coverage
-                        zoneModel.humidity = it.humidity
-                        this@ZonesViewModel.lastUpdatedZone.value = zoneModel
-                    }
-                }
-            }.await()
+            subscribeToChanges()
         }
     }
+
+    private suspend fun CoroutineScope.getModel(): List<ZoneCellModel> {
+        async { mqttClient.connectAsync(getApplication()) }.await()
+        val zonesClimatelist = async {
+            mqttClient.getResponse<List<ZoneClimateData>>(
+                requestTopic = "AllZonesReadingsRequest",
+                responseTopic = "AllZonesReadingResponse"
+            )
+        }.await()
+        val zonesBoilerlist = async {
+            mqttClient.getResponse<List<ZoneBoilerData>>(
+                requestTopic = "AllZonesConfigRequest",
+                responseTopic = "AllZonesConfigResponse"
+            )
+        }.await()
+        val modelList = zonesClimatelist.map {
+            ZoneCellModel(
+                temperature = it.temperature,
+                humidity = it.humidity,
+                coverage = it.coverage,
+                zoneCode = it.zoneCode
+            )
+        }
+        zonesBoilerlist.forEach {
+            val zoneCellModel=modelList.first{a-> a.zoneCode.equals(it.zoneCode,true)}
+            zoneCellModel.regulated=it.regulated
+            zoneCellModel.targetTemperature=it.targetTemperature
+        }
+        return  modelList
+    }
+
+    private suspend fun CoroutineScope.subscribeToChanges() {
+        async {
+            mqttClient.subscribe<ZoneClimateData>("zoneClimateChange") {
+                val zoneModel =
+                    this@ZonesViewModel.zones.value!!.firstOrNull { a -> a.zoneCode.equals(it.zoneCode, true) }
+                if (zoneModel != null) {
+                    zoneModel.temperature = it.temperature
+                    zoneModel.coverage = it.coverage
+                    zoneModel.humidity = it.humidity
+                    this@ZonesViewModel.lastUpdatedZone.value = zoneModel
+                }
+            }
+        }.await()
+        async {
+            mqttClient.subscribe<ZoneBoilerData>("zoneBoilerChange") {
+                val zoneModel =
+                    this@ZonesViewModel.zones.value!!.firstOrNull { a -> a.zoneCode.equals(it.zoneCode, true) }
+                if (zoneModel != null) {
+                    zoneModel.targetTemperature = it.targetTemperature
+                    zoneModel.regulated = it.regulated
+                    this@ZonesViewModel.lastUpdatedZone.value = zoneModel
+                }
+            }
+        }.await()
+    }
+
     fun disconnect(){
         mqttClient.disconnect()
     }
