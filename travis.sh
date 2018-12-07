2911b3f813ed28f2e4ae7fb203b596b03bb64e44
miguel <mig.ruiz@gmail.com>
2018-12-07 10:52:02 +0000

    triggering buid

--- travis.sh
+++ travis.sh
@@ -5,3 +5,4 @@ docker pull $HUBNAME || true
 docker build -t $HUBNAME  -f docker/Dockerfile docker/. --cache-from $HUBNAME 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
+docker run  --rm -v \$(pwd):/home/app  migruiz/climacompiler bash -c 'cd /home/app && chmod +x gradlew && ./gradlew clean'
