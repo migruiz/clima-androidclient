24720cc89f82962eef121bd22f9c6ce16e4634b1
miguel <mig.ruiz@gmail.com>
2018-12-07 10:18:51 +0000

    fff

--- travis.sh
+++ travis.sh
@@ -2,6 +2,6 @@
 set -ev
 HUBNAME="migruiz/climacompiler"
 docker pull $HUBNAME || true
-docker build -f docker/Dockerfile docker/. --cache-from $HUBNAME -t $HUBNAME  . 
+docker build -t $HUBNAME  -f docker/Dockerfile docker/. --cache-from $HUBNAME 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
